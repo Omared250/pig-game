332a3d9be8a7714b06dea7aff289f305db3934d1
Omar <omared250@gmail.com>
2021-12-08 12:18:33 +0100

    switching players

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting the scores elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePLayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -31,10 +35,13 @@ btnRoll.addEventListener('click', function() {
     if (dice !== 1) {
         // Add the dice to the current score
         currentScore += dice;
-        current0El.textContent = currentScore;
+        document.getElementById(`current--${activePLayer}`).textContent = currentScore;
     } else {
         // Switch to next player
-        
+        document.getElementById(`current--${activePLayer}`).textContent = 0;
+        activePLayer = activePLayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');    
+        player1El.classList.toggle('player--active');    
     }
-
 })
